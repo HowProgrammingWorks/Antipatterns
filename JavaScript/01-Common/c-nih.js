48ffe1953a0b52afc1d5a190587c854261016bea
tshemsedinov <timur.shemsedinov@gmail.com>
2019-07-19 23:06:05 +0300

    Add nih

---
+++ JavaScript/01-Common/c-nih.js
@@ -0,0 +1,11 @@
+'use strict';
+
+// Antipattern: Not Invented Here
+//
+// Advantages
+// - No dependencies - no problems (remember left-pad)
+// - Security
+//
+// Disadvantages
+// - Waste of time
+// - Solution quality
