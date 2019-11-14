c36aaa79b50f5a79aa6aa1be861ff2a263b7a117
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2019-11-14 12:17:10 +0200

    Add solution

--- JavaScript/02-Procedural/4-parameters.js
+++ JavaScript/02-Procedural/4-parameters.js
@@ -38,3 +38,35 @@
     'Stoicism',
   );
 }
+
+// Solution
+{
+  const validatePerson = ({ name, city, birth, dynasty, school }) => {
+    if (name.length < 3) return false;
+    if (!city) return false;
+    if (Number.isNaN(Date.parse(birth))) return false;
+    if (dynasty.length < 5) return false;
+    if (dynasty === school) return false;
+    return true;
+  };
+
+  const serializePerson = ({ name, city, birth, dynasty, school }) => {
+    const date = new Date(birth).toISOString();
+    return `${name} [${dynasty}] from ${city} (${date}) ${school} school`;
+  };
+
+  const registerPerson = person => {
+    const valid = validatePerson(person);
+    if (!valid) throw new Error('Emperor is invalid');
+    const data = serializePerson(person);
+    console.log(data);
+  };
+
+  registerPerson({
+    name: 'Marcus Aurelius',
+    city: 'Rome',
+    birth: '212-04-26',
+    dynasty: 'Nerva-Antonine',
+    school: 'Stoicism',
+  });
+}
